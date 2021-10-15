6c81ba65222331df8ebea33c5466ea6433bb098b
NzkFalconet <khanmuzzama0786@gmail.com>
2021-10-15 21:01:14 +0530

    a

---
+++ 15-10-2021/Greatest Element to the left.cpp
@@ -0,0 +1,40 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ int main()
+ {
+     stack<int>s;
+     vector<int>v{1,3,2,4};
+     vector<int>vec;
+     for(int i=0;i<v.size();++i)
+     {
+         if(s.empty())
+         {
+             vec.push_back(-1);
+         }
+         if(!s.empty() && s.top()>v[i])
+         {
+             vec.push_back(s.top());
+         }
+         if(!s.empty() && s.top()<=v[i])
+         {
+             while(!s.empty() && s.top()<=v[i])
+             {
+                 s.pop();
+             }
+             if(s.empty())
+             {
+                 vec.push_back(-1);
+             }
+             else
+             {
+                 vec.push_back(s.top());
+             }
+         }
+         s.push(v[i]);
+     }
+
+     for(auto it:vec)
+     {
+         cout<<it<<" ";
+     }
+ }
---
+++ 15-10-2021/Stock Span Problem.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    stack<pair<int,int>>s;
+    vector<int>v{100,80,60,70,60,75,85};
+    vector<int>vec;
+    for(int i=0;i<v.size();++i)
+    {
+        if(s.empty())
+        {
+            vec.push_back(-1);
+        }
+        if(!s.empty() && s.top().first>v[i])
+        {
+            vec.push_back(s.top().second);
+        }
+        if(!s.empty() && s.top().first<=v[i])
+        {
+            while(!s.empty() && s.top().first<=v[i])
+            {
+                s.pop();
+            }
+            if(s.empty())
+            {
+                vec.push_back(-1);
+            }
+            else
+            {
+                vec.push_back(s.top().second);
+            }
+        }
+        s.push({v[i],i});
+    }
+    for(int i=0;i<v.size();++i)
+    {
+        vec[i]=i-vec[i];
+    }
+    for(auto it:vec)
+    {
+        cout<<it<<" ";
+
+    }
+}
